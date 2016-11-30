5a1a9ac202e187ec8e2936f8ec89daa16bfc2931
lbm6389 <lbmills@umich.edu>
2016-11-29 19:39:36 -0500

    work on forms and database

---
+++ HomeBeerInventory/HomeBeerInventory/AddBeer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace HomeBeerInventory
+{
+    public partial class AddBeer : Form
+    {
+        string databaseLocation;
+        SQLiteConnection databaseConnection;
+
+        public AddBeer(string dbLoc)
+        {
+            InitializeComponent();
+            databaseLocation = dbLoc;
+            OpenConnection();
+            InitializeBreweryComboBox();
+        }
+
+        private void OpenConnection()
+        {
+            databaseConnection = new SQLiteConnection(string.Format("Data Source={0};Version=3;", databaseLocation));
+            databaseConnection.Open();
+        }
+
+        private void CloseConnection()
+        {
+            databaseConnection.Close();
+            databaseConnection.Dispose();
+        }
+
+        private void addBeerButton_Click(object sender, EventArgs e)
+        {
+            //Check Mandatory Fields
+
+        }
+
+        private void InitializeBreweryComboBox()
+        {
+            string breweriesQuery = "select distinct Brewery from BeerInventory";
+            SQLiteCommand breweriesCommand = new SQLiteCommand(breweriesQuery, databaseConnection);
+            SQLiteDataReader breweriesReader = breweriesCommand.ExecuteReader();
+            if (breweriesReader.HasRows)
+            {
+                while (breweriesReader.Read())
+                {
+                    BreweryComboBox.Items.Add(breweriesReader.GetString(0));
+                }
+            }
+            breweriesReader.Dispose();
+            breweriesCommand.Dispose();
+        }
+    }
+}
---
+++ HomeBeerInventory/HomeBeerInventory/MainWindow.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace HomeBeerInventory
+{
+    public partial class MainWindow : Form
+    {
+        private string databaseLocation;
+        private SQLiteConnection databaseConnection;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            if(Properties.Settings.Default.databaseLocation == string.Empty)
+            {
+                MessageBox.Show("No database. Please create a new database or connect to an existing one from the file menu.");
+            }
+            else
+            {
+                databaseLocation = Properties.Settings.Default.databaseLocation;
+                ConnectToDatabase();
+            }
+        }
+
+        private void addBeerButton_Click(object sender, EventArgs e)
+        {
+            CloseDatabase();
+            AddBeer addBeer = new AddBeer(databaseLocation);
+            addBeer.ShowDialog();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            databaseConnection.Close();
+            Close();
+        }
+
+        private void createNewBeerDatabaseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog databaseLocationDialog = new SaveFileDialog();
+            databaseLocationDialog.Filter = "SQLite Database File|*.sqlite";
+            DialogResult databaseLocationResult = databaseLocationDialog.ShowDialog();
+            if (databaseLocationResult == DialogResult.OK)
+            {
+                SQLiteConnection.CreateFile(string.Format("{0}", databaseLocationDialog.FileName));
+                databaseLocation = databaseLocationDialog.FileName;
+                Properties.Settings.Default.databaseLocation = databaseLocation;
+                Properties.Settings.Default.Save();
+                ConnectToDatabase();
+                CreateDatabaseTable();
+            }
+        }
+
+        private void ConnectToDatabase()
+        {
+            databaseConnection = new SQLiteConnection(string.Format("Data Source={0};Version=3;", databaseLocation));
+            databaseConnection.Open();
+        }
+
+        private void CloseDatabase()
+        {
+            databaseConnection.Close();
+        }
+
+        private void CreateDatabaseTable()
+        {
+            string createTableQuery = @"create table BeerInventory (
+                                            PurchaseDate date,
+                                            Brewery varchar(100),
+                                            BeerName varchar(100),
+                                            Vintage int,
+                                            BeerStyle varchar(100),
+                                            BeerSize varchar(100),
+                                            ContainerFormat varchar(100),
+                                            BottledDate date,
+                                            PurchasePrice float,
+                                            EnjoyByDate date,
+                                            ForTrade char(1),
+                                            EnjoyedDate date,
+                                            TradedDate date)";
+            SQLiteCommand createTableCommand = new SQLiteCommand(createTableQuery, databaseConnection);
+            createTableCommand.ExecuteNonQuery();
+            createTableCommand.Dispose();
+        }
+
+        private void connectToExistingDatabaseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog databaseLocationDialog = new OpenFileDialog();
+            databaseLocationDialog.Filter = "SQLite Database File|*.sqlite";
+            DialogResult databaseLocationResult = databaseLocationDialog.ShowDialog();
+            if (databaseLocationResult == DialogResult.OK)
+            {
+                databaseLocation = databaseLocationDialog.FileName;
+                Properties.Settings.Default.databaseLocation = databaseLocation;
+                Properties.Settings.Default.Save();
+                ConnectToDatabase();
+            }
+        }
+    }
+}
